cc3f2638625e2bb78d328cbf1bda651a704e34e8
leitouran <lionel.u.l@gmail.com>
2019-10-03 13:02:58 -0300

    agrego TP viz de aln y estruc

---
+++ TPs/Visualizacion de Alineamientos y Estructuras/installChimera.sh
@@ -0,0 +1,5 @@
+#sudo wget -O chimera-installer.bin "https://www.cgl.ucsf.edu/chimera/cgi-bin/secure/chimera-get.py?ident=OHeQer2WTaRn%2FONstn1K%2BP9hv1BVQNjx3h543g3jnvYs&file=linux_x86_64%2Fchimera-1.14-linux_x86_64.bin&choice=Notified"
+sudo chmod +x chimera-installer.bin
+sudo ./chimera-installer.bin
+
+
