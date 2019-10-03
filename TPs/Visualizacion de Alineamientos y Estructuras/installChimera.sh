ac40a346b6c67394fb46c587420752fca89b408e
leitouran <lionel.u.l@gmail.com>
2019-10-03 20:17:12 -0300

    arreglo script de instalacion - cambio link de descarga -

--- TPs/Visualizacion de Alineamientos y Estructuras/installChimera.sh
+++ TPs/Visualizacion de Alineamientos y Estructuras/installChimera.sh
@@ -1,4 +1,4 @@
-#sudo wget -O chimera-installer.bin "https://www.cgl.ucsf.edu/chimera/cgi-bin/secure/chimera-get.py?ident=OHeQer2WTaRn%2FONstn1K%2BP9hv1BVQNjx3h543g3jnvYs&file=linux_x86_64%2Fchimera-1.14-linux_x86_64.bin&choice=Notified"
+sudo wget -O chimera-installer.bin "https://www.dropbox.com/s/l2y3njilbmo7la1/chimera-installer.bin?dl=1"
 sudo chmod +x chimera-installer.bin
 sudo ./chimera-installer.bin
 
