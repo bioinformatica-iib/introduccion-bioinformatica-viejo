6e7421751c051599eebf1c1ed9b3418428921415
efenoy <efenoy7@gmail.com>
2020-09-15 19:01:05 -0300

    Update Install.sh

--- TPs/Filogenia/data/Install.sh
+++ TPs/Filogenia/data/Install.sh
@@ -4,11 +4,5 @@ NC='\033[0m'
 echo -e "${RED} Instalando Phylip...${NC}\n\n "
 sudo apt install embassy-phylip
 
-echo -e "${RED} Descargando Dendroscope ...${NC} \n\n"
-wget "https://software-ab.informatik.uni-tuebingen.de/download/dendroscope/Dendroscope_unix_3_7_2.sh" -O ~/Dendroscope_unix_3_7_2.sh
-
-echo -e "${RED} Instalando Dendroscope...${NC}\n\n "
-bash ~/Dendroscope_unix_3_7_2.sh
-
 echo -e "${RED} Instalando Figtree...${NC}\n\n "
 sudo apt install figtree
