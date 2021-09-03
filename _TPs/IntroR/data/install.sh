95732cf30437991bf218f2bf55197619c6288c05
Leonel <leonelatm44@gmail.com>
2021-09-03 18:34:32 -0300

    Add files via upload

--- _TPs/IntroR/data/install.sh
+++ _TPs/IntroR/data/install.sh
@@ -1,8 +1,6 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-echo -e "Tenes una VM de 32 o de 64 Bits? [ingresa '32' o '64', segun corresponda] \n\n"
-read ARCH
 
 echo -e "${RED} INSTALANDO R...${NC}\n\n "
 
@@ -13,18 +11,11 @@ echo -e "${RED} INSTALANDO Rstudio...${NC}\n\n "
 
 sudo rm -f  rstudio*.deb 
 
-ARCH64=`echo ${ARCH} | grep 64 | wc -l`
-ARCH32=`echo ${ARCH} | grep 32 | wc -l`
 
-if [ "${ARCH64}" = "1" ]
-then
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1073-amd64.deb
-fi
 
-if [ "${ARCH32}" = "1" ]
-then
-#Esto hay que hacerlo porque hay unos paquetes viejos que rstudio pide y si instalas la versión nueva no reconoce X_X
+
 sudo apt-get install -y libjpeg62-dev
+sudo apt-get install -y libgit2-dev
 wget http://fr.archive.ubuntu.com/ubuntu/pool/universe/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.5ubuntu1_i386.deb
 wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gst-plugins-base0.10/libgstreamer-plugins-base0.10-0_0.10.36-2_i386.deb
 sudo dpkg -i libgstreamer0.10-0_0.10.36-1.5ubuntu1_i386.deb
@@ -32,7 +23,6 @@ sudo dpkg -i libgstreamer-plugins-base0.10-0_0.10.36-2_i386.deb
 sudo apt-mark hold libgstreamer-plugins-base0.10-0
 sudo apt-mark hold libgstreamer0.10
 wget http://download1.rstudio.org/rstudio-1.1.463-i386.deb
-fi
 
 sudo dpkg -i rstudio*.deb 
 
